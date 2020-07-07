327ecb13da0d6cc33cbe222cde16be0092eb917a
Pascal <pascal.wiese@web.de>
2020-07-07 21:44:04 +0200

    added new script and debug script

---
+++ mesh_depth_to_blender.py
@@ -0,0 +1,126 @@
+import os
+import bpy
+#
+# Input values
+folder = r"/media/z/Work"
+name_mesh = "system.2dm"
+name_dat = "DEPTH_max.dat"
+mode = "depth"
+epsilon = 0.01
+#
+#
+# Global
+valid_modes = ["depth", "wspl"]
+#
+#
+# Creating paths
+path_twodm = os.path.join(folder, name_mesh)
+path_dat = None
+if mode in valid_modes:
+    path_dat = os.path.join(folder, name_dat)
+#
+#
+# Reading Mesh
+verts = []
+edges = [] # can be left empty
+faces = []
+for line in open(path_twodm):
+    if line.startswith("ND"):
+        parts = line.split()
+        verts.append([float(n) for n in parts[2:]])
+        #
+    elif line.startswith("E3T") or line.startswith("E4Q"):
+        parts = line.split()
+        if line.startswith("E3T"):
+            nids = [int(nid) - 1 for nid in parts[2:5]]
+            faces.append(nids)
+        else:
+            nids = [int(n) - 1 for n in line.split()[2:6]]
+            faces.append([nids[0], nids[1], nids[2]])
+            faces.append([nids[0], nids[2], nids[3]])
+#
+#
+x_min = 999999999999999
+y_min = 999999999999999
+z_min = 999999999999999
+x_max = 0
+y_max = 0
+z_max = 0
+#
+for v in verts:
+    if v[0] < x_min:
+        x_min = v[0]
+    if v[1] < y_min:
+        y_min = v[1]
+    if v[2] < z_min:
+        z_min = v[2]
+    if v[0] > x_max:
+        x_max = v[0]
+    if v[1] > y_max:
+        y_max = v[1]
+    if v[2] > z_max:
+        z_max = v[2]
+#
+for i, v in enumerate(verts):
+    verts[i][0] = v[0] - (x_min + x_max) / 2
+    verts[i][1] = v[1] - (y_min + y_max) / 2
+    verts[i][2] = v[2] - z_min
+#
+#
+if mode in valid_modes:
+    # Reading dat
+    wspls = []
+    i = 0
+    for line in open(path_dat):
+        try:
+            val = float(line)
+            if mode == "depth":
+                val = val + verts[i][2]
+            wspls.append(val)
+            i += 1
+        except:
+            pass
+    #
+    # Getting wet nodes
+    wet_node_ids = []
+    faces_wspl = []
+    for eid, (nid1, nid2, nid3) in enumerate(faces):
+        is_wet = [wspls[nid1] > verts[nid1][2] + epsilon, wspls[nid2] > verts[nid2][2] + epsilon, wspls[nid3] > verts[nid3][2] + epsilon]
+        if all(is_wet):
+            faces_wspl.append([nid1, nid2, nid3])
+            wet_node_ids += [nid1, nid2, nid3]
+    wet_node_ids = list(set(wet_node_ids))
+    #
+    verts_wspl_wet = [[verts[nid][0], verts[nid][1], wspls[nid]] for nid in wet_node_ids]
+    #
+    # Changind node ids
+    old_to_new_nid = {o: n for o, n in zip(wet_node_ids, range(len(wet_node_ids)))}
+    faces_wspl_wet = []
+    for eid, (nid1, nid2, nid3) in enumerate(faces_wspl):
+        faces_wspl_wet.append([old_to_new_nid[nid1], old_to_new_nid[nid2], old_to_new_nid[nid3]])
+#
+#
+# Create mesh object
+mesh = bpy.data.meshes.new("Mesh")
+obj = bpy.data.objects.new("MeshObject", mesh)
+# Blender 2.8
+bpy.context.collection.objects.link(obj)
+bpy.context.view_layer.objects.active = obj
+obj.select_set(1)
+mesh = obj.data
+mesh.from_pydata(verts, edges, faces)
+mesh.update()
+#
+if mode in valid_modes:
+    # Create mesh object
+    meshwspl = bpy.data.meshes.new("Wspl")
+    objwspl = bpy.data.objects.new("WsplObject", meshwspl)
+    # Blender 2.8
+    bpy.context.collection.objects.link(objwspl)
+    bpy.context.view_layer.objects.active = objwspl
+    objwspl.select_set(1)
+    meshwspl = objwspl.data
+    meshwspl.from_pydata(verts_wspl_wet, [], faces_wspl_wet)
+    meshwspl.update()
+
+
